c5a4cb2e5add026de0a9ceb0251e1db4f7b79898
WizzOFF <maher_off@proton.me>
2025-03-12 16:14:54 +0100

    manipulate_text_with_strings.c: use_the_char_type_to_store_a_letter + scanf

--- manipulate_text_with_strings.c
+++ manipulate_text_with_strings.c
@@ -1,8 +1,19 @@
-
+/*
 int main(int argc, char *argv[])
 {
     char lettre = 'A';
 
+    printf("%c\n", lettre);
+
+    return 0;
+}
+*/
+
+int main(int argc, char *argv[])
+{
+    char lettre = 0;
+
+    scanf("%c", &lettre);
     printf("%c\n", lettre);
 
     return 0;
