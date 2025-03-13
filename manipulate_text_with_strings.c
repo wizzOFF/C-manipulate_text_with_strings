9730b77e99b00ec39b3d211821947c9c18b6dcb1
WizzOFF <maher_off@proton.me>
2025-03-13 16:54:39 +0100

    manipulate_text_with_strings.c: calculate the length of a string with strlen / function longueurChaine

--- manipulate_text_with_strings.c
+++ manipulate_text_with_strings.c
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 */
-
+/*
 int main(int argc, char *argv[])
 {
     char chaine[] = "Salut";
@@ -67,4 +67,37 @@ int main(int argc, char *argv[])
     printf("La chaine %s fait %d caracteres de long", chaine, longueurChaine);
 
     return 0;
+}
+*/
+
+int longueurChaine(const char* chaine);
+
+int main(int argc, char *argv[])
+{
+    char chaine[] = "Salut";
+    int longueur = 0;
+
+    longueur = longueurChaine(chaine);
+
+    printf("La chaine %s fait %d caracteres de long", chaine, longueur);
+
+    
+    return 0;
+}
+
+int longueurChaine(const char* chaine)
+{
+    int nombreDeCaracteres = 0;
+    char caractereActuel = 0;
+
+    do
+    {
+        caractereActuel = chaine[nombreDeCaracteres];
+        nombreDeCaracteres++;
+    }
+    while(caractereActuel != '\0'); // On boucle tant qu'on n'est pas arrivé à l'\0
+
+    nombreDeCaracteres--; // On retire 1 caractère de long pour ne pas compter le caractère \0
+
+    return nombreDeCaracteres;
 }
